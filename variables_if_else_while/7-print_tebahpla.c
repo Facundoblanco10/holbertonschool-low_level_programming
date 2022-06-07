80426b2a5bf20fc4e4d13fa8c5f9df7e077e3944
Facundoblanco10 <facub1011@gmail.com>
2022-06-07 15:22:08 -0700

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'z'; let >= 'a'; let--)
+	{
+		putchar(let);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
