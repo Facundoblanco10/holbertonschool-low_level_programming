4b9aea09200d02ce867c7ca8d0c682eccc301d73
Facundoblanco10 <facub1011@gmail.com>
2022-06-07 15:30:33 -0700

    hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+	int let;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		}
+	for (let = 'a'; let <= 'f'; let++)
+	{
+		putchar(let);
+	}
+		putchar('\n');
+	return (0);
+}
