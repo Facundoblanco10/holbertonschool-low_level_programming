8917207da4508f5ab34c954d252cd9ed28b7a490
Facundoblanco10 <facub1011@gmail.com>
2022-06-09 14:45:35 -0700

    Absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _abs -  function that computes the absolute value of an integer.
+* @a: variable
+* Return: absolute value
+*/
+int _abs(int a)
+{
+	return (abs(a));
+}
