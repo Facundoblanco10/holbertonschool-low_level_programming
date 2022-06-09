5d8bbf90061968c4c5a335d930f6541e32ab2cac
Facundoblanco10 <facub1011@gmail.com>
2022-06-09 10:37:18 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - entry point
+* @c: character
+* Return: 1 (lowercase) 0 (if is not)
+*/
+int _islower(int c)
+{
+	if(c >= 97 && c <= 122) 
+		return (1);
+	else
+		return (0);
+}
