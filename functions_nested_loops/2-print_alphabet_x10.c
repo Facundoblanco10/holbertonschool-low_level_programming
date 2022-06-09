3d03780270c21b5a1a79edb59b7dbd5a7801a81b
Facundoblanco10 <facub1011@gmail.com>
2022-06-09 07:15:42 -0700

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - entry point
+*/
+void print_alphabet_x10(void)
+{
+	char let;
+	int count = 0;
+	for (count = 0; count <=10; count++)
+	{
+		for (let = 'a'; let <= 'z'; let++)
+		{
+			_putchar(let);
+		}
+			_putchar('\n');
+	}
+}
