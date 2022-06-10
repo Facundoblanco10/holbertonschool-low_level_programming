cf5d24cce56e61aa9ed40e0a3e012168f5e889d8
Facundoblanco10 <facub1011@gmail.com>
2022-06-09 17:29:02 -0700

    tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+
+void times_table(void)
+{
+	int num, mult;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (mult = 0; mult <= 9; mult++)
+		{
+			if ((num * mult) <= 9){
+				_putchar((num * mult) %10 + '0');
+			}
+			else
+			{
+				_putchar((num * mult) /10 + '0');
+				_putchar((num * mult) %10 + '0');
+			}
+				if (mult ==9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
