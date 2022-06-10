bc1ae390a1864e0488bad9272860f93bca892f80
Facundoblanco10 <facub1011@gmail.com>
2022-06-09 17:38:38 -0700

    tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,21 +14,21 @@ void times_table(void)
 	{
 		for (mult = 0; mult <= 9; mult++)
 		{
-			if ((num * mult) <= 9){
+			if ((num * mult) <= 9)
+			{
 				_putchar(' ');
-				_putchar((num * mult) %10 + '0');
+				_putchar(((num * mult) % 10) + '0');
 			}
 			else
 			{
-				_putchar((num * mult) /10 + '0');
-				_putchar((num * mult) %10 + '0');
+				_putchar(((num * mult) / 10) + '0');
+				_putchar(((num * mult) % 10) + '0');
 			}
-				if (mult ==9)
+			if (mult == 9)
 				continue;
 			_putchar(',');
 			_putchar(' ');
 		}
-		_putchar('$');
 		_putchar('\n');
 	}
 }
