fddf74618323b2296e5b54a2460274c77cc91ed4
Facundoblanco10 <facub1011@gmail.com>
2022-06-10 07:22:28 -0700

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*
+*
+*
+*/
+
+void print_to_98(int n)
+{
+	int l;
+
+	if (n != 98)
+	{
+		for (l = n; l < 98; l++)
+		{
+			printf("%d, ", l);
+		}
+		for (l = n; l > 98; l--)
+		{
+			printf("%d, ", l);
+		}
+	}
+	if (n == 98)
+		printf("%d\n", n);
+}
