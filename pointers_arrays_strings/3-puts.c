fd742ac536f842932794d06a4ab8d24fc9b35819
Facundoblanco10 <facub1011@gmail.com>
2022-06-14 12:14:52 -0700

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,15 +9,15 @@
 void _puts(char *str)
 {
 	char c;
-	int i = -1;
+	int i = 0;
 
 	if (*(str) != 00)
 		{
-		while (c != '\0')
+		while (*(str + i +1) != '\0')
 		{
-			i++;
 			c = *(str + i);
 			_putchar(c);
+			i++;
 		}
 		}
 		_putchar('\n');
