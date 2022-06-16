568ff419422c7bc75a481f6ae09a54be1329637a
Facundoblanco10 <facub1011@gmail.com>
2022-06-16 11:26:35 -0700

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcpy - entry point
+* @dest: variable
+* @src: variable
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+		if (src[i] == '\0')
+		{
+			dest[i] = src[i];
+		}
+	}
+	return(dest);
+}
