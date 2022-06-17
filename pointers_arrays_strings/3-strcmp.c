dd94055e7bbe1e2b8062ef0f50cadd5d9ed3a7a1
Facundoblanco10 <facub1011@gmail.com>
2022-06-17 10:08:41 -0700

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,8 +13,10 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i])
-		i++;
+		{
+			return (s1[i] - s2[i]);
+		}
+			i++;
 	}
 	return (0);
 }
