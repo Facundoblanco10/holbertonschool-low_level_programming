77332aaba195231637de3301c2c634c4f865a52a
Facundoblanco10 <facub1011@gmail.com>
2022-06-17 16:08:46 -0700

    Look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,8 +2,8 @@
 
 /**
 * string_toupper - entry point
-*
 * @t: character
+* Return: always 0
 */
 
 char *string_toupper(char *t)
@@ -16,5 +16,5 @@ char *string_toupper(char *t)
 			t[i] -= 32;
 		i++;
 	}
-	return (t);
+	return (0);
 }
