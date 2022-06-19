2ced2f2f1cd5b9d493bb393de0c76c43028dca90
Facundoblanco10 <facub1011@gmail.com>
2022-06-19 14:59:39 -0700

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,16 +1,26 @@
 #include "main.h"
 
 /**
+* print_chessboard - entry point
 *
-*
-*
+* @a: pieces and spaces
 *
 */
 
 void print_chessboard(char (*a)[8])
 {
+	int b = 0;
 	int c = 0;
 
-	while (c <= 8)
+	while (b < 8)
 	{
-		if (a[c] >=
+		c = 0;
+		while (c < 8)
+		{
+			_putchar(a[b][c]);
+			c++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
