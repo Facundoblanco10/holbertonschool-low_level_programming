bac2b175affa50f8ebc2f130f744b6079c386886
Facundoblanco10 <facub1011@gmail.com>
2022-06-20 10:18:07 -0700

    Pow

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _pow_recursion
+* _pow_recursion - entry point
 * @x: base
 * @y: exponent
 * Return: result
@@ -12,6 +12,10 @@ int _pow_recursion(int x, int y)
 	if (y > 0)
 	{
 		return (_pow_recursion(x, y - 1) * x);
+	}
+	if (y < 0)
+	{
+		return (-1);
 	}
 	return (1);
 }
