835f3b87d205d09397320bb756f1312caa69f816
Facundoblanco10 <facub1011@gmail.com>
2022-06-21 08:08:52 -0700

    fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
-*
-*
-*
+* main - entry point
+* @argc: argc
+* @argv: argv
+* Return: 0
 */
 
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	
+
 	if (argc == 2)
 	{
 		return (1);
