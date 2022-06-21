4e85637dfbd27a18ada0ec07ccb7cdcb8043a5d4
Facundoblanco10 <facub1011@gmail.com>
2022-06-21 09:01:03 -0700

    Args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* @argc: argc
+* @argv: argv
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
