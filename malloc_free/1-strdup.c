b50e680db30755cce9f40fdc74dd6ce58e10b21a
Facundoblanco10 <facub1011@gmail.com>
2022-06-23 12:26:19 -0700

    strdup

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - entry point
+* @str: string
+* Return: return NULL or a pointer to the duplicated string
+*/
+
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+		s = malloc(i * 2);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
