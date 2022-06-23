89fe4ac40d0cde8bfb3fc0eaa5085650481d2885
Facundoblanco10 <facub1011@gmail.com>
2022-06-23 09:31:56 -0700

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,11 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i;
 
-	s = malloc(size);
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	s = malloc(size);
 	i = 0;
 	while (i < size)
 	{
