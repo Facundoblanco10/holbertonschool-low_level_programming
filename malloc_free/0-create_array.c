2c54d454b5e715e23b73116c110f9af85ce7b28e
Facundoblanco10 <facub1011@gmail.com>
2022-06-23 09:15:33 -0700

    Create array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+* create_array - entry point
+* @size: size
+* @c: char
+* Return: return NULL or a pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(size);
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
