2a2c76e3ebc3d8bdf5842fb03c59e3d543badafe
Facundoblanco10 <facub1011@gmail.com>
2022-06-24 15:42:41 -0700

    malloc_checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,8 @@
 
 /**
 * malloc_checked - allocate memory using malloc
-*
-* @b: 
+* @b: size
+* Return: a pointer to the allocated memory
 */
 
 void *malloc_checked(unsigned int b)
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 	void *i;
 
 	i = malloc(b);
-	if(i == NULL)
+	if (i == NULL)
 		exit(98);
 	return (i);
 }
