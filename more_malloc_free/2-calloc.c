be14ab9bb0b6628de783692f5f53b941f3a47d03
Facundoblanco10 <facub1011@gmail.com>
2022-06-26 11:07:02 -0700

    Calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ar == 0)
 		return (NULL);
 
-	for (i = 0; i < size * sizeof(unsigned int); i++)
+	for (i = 0; i < size * sizeof(nmemb); i++)
 	{
 		ar[i] = 0;
 	}
