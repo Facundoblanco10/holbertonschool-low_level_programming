1defaec7859711a2acffc02a809b8cf22fde4290
Facundoblanco10 <facub1011@gmail.com>
2022-06-28 15:58:38 -0700

    new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* new_dog - new dog
+* @name: name
+* @age: age
+* @owner: owner
+* Return: 0
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
