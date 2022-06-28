8ed78cd0ce44df67192b6c4f6785448b9cdb28fc
Facundoblanco10 <facub1011@gmail.com>
2022-06-28 12:52:13 -0700

    Print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,19 +3,15 @@
 #include <stdlib.h>
 
 /**
-* init_dog - functiom that initialize a variable of type struct dog
-* struct dog - struct
+* print_dog - print a struct dog
 * @d: d
-* @name: name
-* @age: age
-* @owner: owner
 */
 
 void print_dog(struct dog *d)
 {
 	if (d == 0)
 		exit(EXIT_SUCCESS);
-	d->name == NULL ? printf("(nil)") : printf("Name: %s\n", d->name);
+	d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 	d->age == 0 ? printf("Age: %f\n", d->age) : printf("Age: %f\n", d->age);
-	d->owner == NULL ? printf("(nil)") : printf("Owner: %s\n", d->owner);
+	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
