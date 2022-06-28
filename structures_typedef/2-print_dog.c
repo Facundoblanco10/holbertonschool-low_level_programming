27a66b21e8eaf9415a52f018a7b881553913389d
Facundoblanco10 <facub1011@gmail.com>
2022-06-28 12:45:54 -0700

    Print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,6 +16,6 @@ void print_dog(struct dog *d)
 	if (d == 0)
 		exit(EXIT_SUCCESS);
 	d->name == NULL ? printf("(nil)") : printf("Name: %s\n", d->name);
-	d->age == 0 ? printf("(nil)") : printf("Age: %f\n", d->age);
+	d->age < 1 ? printf("(nil)") : printf("Age: %f\n", d->age);
 	d->owner == NULL ? printf("(nil)") : printf("Owner: %s\n", d->owner);
 }
