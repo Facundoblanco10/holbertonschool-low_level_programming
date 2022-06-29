aa9635c80627ac527154c2bf78c76849ec1be876
Facundoblanco10 <facub1011@gmail.com>
2022-06-29 16:45:24 -0700

    Array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+* array_iterator - function that executes a function
+* @array: array
+* @size: size
+* @action: action
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action != 0)
+	{
+		for (i = 0; i < size; i++)
+		action(array[i]);
+	}
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef POINTERS
 #define POINTERS
+#include <stddef.h>
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 #endif
