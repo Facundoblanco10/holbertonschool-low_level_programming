3704b7bf34f425748e180172655e4eb2e753c0c6
Facundoblanco10 <facub1011@gmail.com>
2022-06-29 12:16:58 -0700

    Print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that print a name
+* @name: name
+* @f: function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	 f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef POINTERS
+#define POINTERS
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+#endif
