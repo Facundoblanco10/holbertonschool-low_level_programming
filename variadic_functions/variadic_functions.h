660b848e772f394c2ad70a40fb1123e4e5410f9f
Facundoblanco10 <facub1011@gmail.com>
2022-07-01 11:02:57 -0700

    Print Numbers

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC
 #define VARIADIC
 #include <stdarg.h>
+#include <stdio.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 #endif
