bf667d63366f4ccf00027edf4d0efae93db811cc
Facundoblanco10 <facub1011@gmail.com>
2022-07-13 10:57:00 -0700

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,8 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
 	new->str = strdup(str);
 	new->len = strlen(str);
