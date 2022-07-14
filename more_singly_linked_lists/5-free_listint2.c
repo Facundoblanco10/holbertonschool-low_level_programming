78c0cd9f75356222b4086154cf769c39460ce95d
Facundoblanco10 <facub1011@gmail.com>
2022-07-14 16:37:12 -0700

    free list2

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* free_listint2 - free a list
+* @head: pointer of a pointer to the start
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
