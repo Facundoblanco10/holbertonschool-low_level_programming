ebefef78a273635d6efde744803767ee6d4a2645
Facundoblanco10 <facub1011@gmail.com>
2022-07-14 16:16:23 -0700

    task4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+* free_listint - free a list
+* @head: pointer to the start
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
