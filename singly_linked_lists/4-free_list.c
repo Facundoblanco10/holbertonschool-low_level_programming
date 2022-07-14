ba816504414ecfb60c2b15f5ea2c77ded4cbb5ee
Facundoblanco10 <facub1011@gmail.com>
2022-07-14 07:22:45 -0700

    free

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,11 +7,13 @@
 
 void free_list(list_t *head)
 {
+	list_t *temp;
+
 	while (head)
 	{
 		free(head->str);
-		free(head->next);
-		head = head->next;
-	}
+		temp = head->next;
 		free(head);
+		head = temp;
+	}
 }
