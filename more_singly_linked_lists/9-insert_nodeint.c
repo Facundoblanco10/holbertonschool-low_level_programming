168c20f01c9b186ed7b9411b1d4aa287ba30562a
Facundoblanco10 <facub1011@gmail.com>
2022-07-15 12:16:25 -0700

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	bef = *head;
-	while (bef && i < (idx - 1))
+	while (bef && i != (idx - 1))
 	{
 		if (!bef->next)
 			return (NULL);
