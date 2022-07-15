df6407c4c95b9afd0f520125eeca3cab786b86e2
Facundoblanco10 <facub1011@gmail.com>
2022-07-15 11:10:29 -0700

    Insert nodeint

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 		}
 	}
+	else
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	return (new);
