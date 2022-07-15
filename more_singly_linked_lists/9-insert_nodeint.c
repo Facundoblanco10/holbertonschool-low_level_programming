92cef9d84afff5fcbe852ea0833028b1e8b5fdee
Facundoblanco10 <facub1011@gmail.com>
2022-07-15 12:23:14 -0700

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+	new->n = n;
 	if (idx == 0)
 	{
-		new->n = n;
 		new->next = *head;
 		*head = new;
 		return (new);
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	bef = *head;
 	while (bef && i != (idx - 1))
 	{
-		if (!bef || !bef->next)
+		if (!bef->next)
 			return (NULL);
 		i++;
 		bef = bef->next;
