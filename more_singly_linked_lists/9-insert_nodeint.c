3edb35472ef766ff610ef07f1f72d0dfcbc45c23
Facundoblanco10 <facub1011@gmail.com>
2022-07-15 11:44:01 -0700

    Insert nodeint

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		bef = *head;
 		aft = *head;
-		while (bef && i < idx)
+		while (aft && i < idx)
 		{
 			i++;
 			aft = aft->next;
