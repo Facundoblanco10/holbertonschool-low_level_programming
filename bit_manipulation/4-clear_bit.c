0b33503dca6a84863e921498f1759e0cd5fc1f62
Facundoblanco10 <facub1011@gmail.com>
2022-07-19 09:09:15 -0700

    100

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* set_bit - set the value of a bit to 1 at given index
+* @n: number
+* @index: starting from 0
+* Return: 1 if it worked or -1 if an error occurred
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 64)
+	{
+		*n &= ~(1 << index);
+		return (1);
+	}
+	return (-1);
+}
