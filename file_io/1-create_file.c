cc37d84afe648ee251a30b5dc83feb76e4e72366
Facundoblanco10 <facub1011@gmail.com>
2022-07-20 15:20:43 -0700

    create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: is the name of the file to create
+* @text_content: is a NULL terminated string to write to the file
+* Return: 1 on success or -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[i])
+		i++;
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
