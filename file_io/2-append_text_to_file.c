165ee45b0c706a810b647f254b931ba647204d24
Facundoblanco10 <facub1011@gmail.com>
2022-07-20 15:47:38 -0700

    file append

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* append_text_to_file - creates a file
+* @filename: is the name of the file to create
+* @text_content: is a NULL terminated string to write to the file
+* Return: 1 on success or -1 on failure
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		return (1);
+	while (text_content[i])
+		i++;
+	write(fd, text_content, i);
+	close(fd);
+	return (1);
+}
