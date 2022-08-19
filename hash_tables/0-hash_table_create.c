5a4fed6128bc6a0f26e415e116d56a6852e98d8d
Facundoblanco10 <facub1011@gmail.com>
2022-08-19 10:26:39 -0700

    create a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,8 +9,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *n_ht;
 
 	n_ht = malloc(sizeof(hash_table_t));
+	if (!n_ht)
+		return (NULL);
 	n_ht->size = size;
 	n_ht->array = malloc(sizeof(hash_node_t *) * size);
-	
+	if (!n_ht)
+		return (NULL);
 	return (n_ht);
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+#ifndef HASH_TABLE
+#define HASH_TABLE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,9 +13,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,7 +28,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+#endif
